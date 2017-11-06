eadc1ecfe0da0f4f58caca7554101da5227d8c8e
Sebastián Mancilla <smancill@jlab.org>
2017-11-05 22:16:15 -0300

    travis: cleanup extra environment variables

--- .travis/install.sh
+++ .travis/install.sh
@@ -33,15 +33,18 @@ if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
     ZMQ_VERSION=4.1.6
     PROTO_URL=https://github.com/google/protobuf/releases/download/v${PROTO_VERSION}/protobuf-${PROTO_VERSION}.tar.gz
     ZMQ_URL=https://github.com/zeromq/zeromq4-1/releases/download/v${ZMQ_VERSION}/zeromq-${ZMQ_VERSION}.tar.gz
-    mkdir -p "${DEPS_DIR}/proto" "${DEPS_DIR}/zmq"
+
+    PROTO_SRC_DIR=${TRAVIS_BUILD_DIR}/deps/proto
+    ZMQ_SRC_DIR=${TRAVIS_BUILD_DIR}/deps/zmq
+    mkdir -p "${PROTO_SRC_DIR}" "${ZMQ_SRC_DIR}"
 
     echo "Installing Protoco Buffers from sources..."
-    wget --no-check-certificate --quiet -O - "${PROTO_URL}" | tar --strip-components=1 -xz -C "${DEPS_DIR}/proto"
-    (cd "${DEPS_DIR}/proto" && ./configure --prefix="${INSTALL_DIR}" && make -j"$JOBS" && make install)
+    wget --no-check-certificate --quiet -O - "${PROTO_URL}" | tar --strip-components=1 -xz -C "${PROTO_SRC_DIR}"
+    (cd "${PROTO_SRC_DIR}" && ./configure --prefix="${INSTALL_DIR}" && make && make install)
 
     echo "Installing ZMQ from sources..."
-    wget --no-check-certificate --quiet -O - "${ZMQ_URL}"   | tar --strip-components=1 -xz -C "${DEPS_DIR}/zmq"
-    (cd "${DEPS_DIR}/zmq"   && ./configure --prefix="${INSTALL_DIR}" && make -j"$JOBS" && make install)
+    wget --no-check-certificate --quiet -O - "${ZMQ_URL}"   | tar --strip-components=1 -xz -C "${ZMQ_SRC_DIR}"
+    (cd "${ZMQ_SRC_DIR}"   && ./configure --prefix="${INSTALL_DIR}" && make && make install)
 else
     brew install zeromq
     brew install protobuf
